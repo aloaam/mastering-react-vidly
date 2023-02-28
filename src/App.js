00bc9a21c07e2b90f84d22dd0b047798bd33f114
= <=>
2023-02-28 16:20:41 +0100

    Video Component exercise, without conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { getMovies } from "./fakeMovieService";
+import React, { useState } from "react";
 
 function App() {
+  const [movies, setMovies] = useState(getMovies());
+
+  const handleDeletion = (id) => {
+    setMovies(movies.filter((movie) => movie._id != id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main className="container m-5">
+        <h1>Showing {movies.length} movies in the databse.</h1>
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map((m, ix) => {
+              return (
+                <tr key={ix}>
+                  <th scope="row">{m.title}</th>
+                  <td>{m.genre["name"]}</td>
+                  <td>{m.numberInStock}</td>
+                  <td>{m.dailyRentalRate}</td>
+                  <td>
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      onClick={() => handleDeletion(m._id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </main>
     </div>
   );
 }
